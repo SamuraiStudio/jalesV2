cbb270c91786d5b6789a82dfb99b346d5863f607
Allan Castro <allan.ocastrocruz@gmail.com>
2021-01-23 18:35:42 -0600

    header/navbar import

--- _header.php
+++ _header.php
@@ -1,21 +1,23 @@
 <?php session_start(); ?>
 
 <header>
-    <!-- Encabezado (color + logo) -->
     <div class="py-2" style="background: #FFFFFF;">
         <div class="container">
-            <center><a href="empleos_publico.php"><img src="assets/img/Logo/color.png" class="img-fluid mr-3" style="width: 120px; height: 85px;"></a></center>
+            <center><a href="empleos_usuarios.php"><img src="assets/img/Logo/color.png" class="img-fluid mr-3" style="width: 120px; height: 85px;"></a></center>
         </div>
     </div>
 
-    <!--------------------------------------------------- Barra de navegación -------------------------------------------------->
-    <nav id="navbar_top" class="navbar navbar-expand-lg navbar-dark" style="background-color: #000000;">
+    <!--Barra de navegación-->
+    <nav id="navbar_top" class="navbar navbar-expand-lg navbar-dark" style="background: #000000;">
         <div class="container">
-            <span><a href="empleos_publico.php"><img src="assets/img/Titulo/jale_b_sp_1.png" class="img-fluid" style="width: 150px; height: 65px;"></a></span>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main_nav" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <span><a href="empleos_usuarios.php"><img src="assets/img/Titulo/jale_b_sp_1.png" class="img-fluid" style="width: 150px; height: 65px;"></a></span>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main_nav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
             <div class="collapse navbar-collapse texto" id="main_nav">
                 <ul class="navbar-nav mr-auto">
-                    <!--Buscador-->
+
+                    <!--Buscador   -->
                     <li class="nav-item">
                         <form class="form-inline my-2 my-lg-0">
                             <input class="form-control mr-sm-2" type="search" placeholder="Busca un empleo" aria-label="Search" style="border-radius: 50px;">
@@ -24,8 +26,14 @@
                     </li>
                 </ul>
                 <ul class="navbar-nav">
-                    <li class="nav-item"><a class="nav-link" href="register_user.php">Registrarse</a></li>
-                    <li class="nav-item"><a class="nav-link" href="login.php">Iniciar Sesión</a></li>
+                    <!-- Espaciado -->
+                    <li class="nav-item"><a class="nav-link" href="empleos_usuarios.php">Buscar trabajo</a></li>
+                    <li class="nav-item"><a class="nav-link" href="register_empleo.php">Generar Trabajo</a></li>
+                    <li class="nav-item"><a class="nav-link" href="https://testjales.samuraistudio.com.mx/" target="_blank">Foro</a></li>
+                    <li class="nav-item"><a class="nav-link" href="publicaciones_user.php">Mis publicaciones</a></li>
+                    <li class="nav-item"><a class="nav-link" href="profile_user.php">Mi perfil</a></li>
+                    <!-- TODO: logout -->
+                    <li class="nav-item"><a class="nav-link" href="empleos_publico.php"><span class="fas fa-door-open" style="font-size: 20px;"></span></a></li>
                 </ul>
             </div>
         </div>
--- register_empleo.php
+++ register_empleo.php
@@ -56,42 +56,8 @@
     </style>
 
     <!---------------------------------------------------MENÚ / BARRA DE NAVEGACIÓN -------------------------------------------------->
-    <header>
-      <div class="py-2" style="background: #FFFFFF;">
-        <div class="container">
-          <center><a href="empleos_usuarios.php"><img src="assets/img/Logo/color.png" class="img-fluid mr-3" style="width: 120px; height: 85px;"></a></center>
-        </div>
-      </div>
-      <nav id="navbar_top" class="navbar navbar-expand-lg navbar-dark" style="background: #080000;">
-        <div class="container">
-          <span><a href="empleos_usuarios.php"><img src="assets/img/Titulo/jale_b_sp_1.png" class="img-fluid" style="width: 150px; height: 65px;"></a></span>
-          <br>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main_nav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse texto" id="main_nav">
-            <!--Botones de navegación (Foro, buscar y generar trabajo)-->
-              <ul class="navbar-nav ml-auto">
-                <!--Buscador-->
-                <li class="nav-item">
-                  <form class="form-inline my-2 my-lg-0">
-                      <input class="form-control mr-sm-2" type="search" placeholder="Busca un empleo" aria-label="Search"  style= "border-radius: 50px;">
-                      <button class="btn text-white btn-lg my-2 my-sm-0" type="submit" style="background: #95140A;  border-radius: 50px;">Buscar</button>
-                  </form>
-                </li>
-                <!-- Espaciado -->
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                <li class="nav-item"><a class="nav-link" href="empleos_usuarios.php">Buscar trabajo</a></li>
-                <li class="nav-item"><a class="nav-link" href="register_empleo.php">Generar Trabajo</a></li>
-                <li class="nav-item"><a class="nav-link" href="https://testjales.samuraistudio.com.mx/" target="_blank">Foro</a></li>
-                <li class="nav-item"><a class="nav-link" href="publicaciones_user.php">Mis publicaciones</a></li>
-                <li class="nav-item"><a class="nav-link" href="profile_user.php">Mi perfil</a></li>
-                <li class="nav-item"><a class="nav-link" href="empleos_publico.php"><span class="fas fa-door-open" style="font-size: 20px;"></span></a></li>
-              </ul>
-          </div>
-        </div>
-      </nav>
-    </header>
+    <?php include "_header.php"; ?>
+    
     <!--CUERPO-->
     <body style="background: #E6E1E1;"> <!-- Background gris-->
 

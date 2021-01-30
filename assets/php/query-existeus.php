7e490a918156f96f10196acb1a49db3528d2b8c4
Allan Castro <allan.ocastrocruz@gmail.com>
2021-01-30 16:10:23 -0600

    cambio a declaraciones preparadas

--- assets/php/query-existeus.php
+++ assets/php/query-existeus.php
@@ -7,13 +7,13 @@ $pdo = $db->connect();
 
 sleep(1);
 if (isset($_POST)) {
-    $username = (string)$_POST['correo'];
+    $email = (string)$_POST['correo'];
 
-    $result = $pdo->query(
-        'SELECT * FROM usuario WHERE correo = "'.strtolower($username).'"'
-    );
+    $query = "SELECT * FROM usuario WHERE correo = ?";
+    $stmt = $pdo->prepare($query);
+    $stmt->execute([$email]);
 
-    if ($result->fetchColumn() > 0) {
+    if ($stmt->rowCount() > 0) {
         echo '<div class="alert alert-danger"><strong>Oh no!</strong> Correo electrónico ya en uso, cambialo porfavor.</div>';
     } else {
         echo '<div class="alert alert-success"><strong>Enhorabuena!</strong> Correo electrónico disponible, continua.</div>';

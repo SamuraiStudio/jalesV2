0df91a83ef0e07a0b6b9e4558492b929ac223857
Allan Castro <allan.ocastrocruz@gmail.com>
2021-01-31 18:30:17 -0600

    inserta direccion en la bd

--- assets/php/registro-user.php
+++ assets/php/registro-user.php
@@ -15,6 +15,7 @@ if (isset($_POST['email'])) {
 
   // Constante. Nombre de la tabla de usuarios
   $USER_TABLE = 'usuario';
+  $DIRECCION_TABLE = 'direccion';
 
   // Datos POST de entrada
   $imageHandler = new ImageHandler($_FILES['inpFile']);
@@ -33,49 +34,64 @@ if (isset($_POST['email'])) {
   $des = $_POST['descripcion'];
   $sex = $_POST['genero'];
   $facebook = $_POST['facebook'];
+  $estado = $_POST['estado'];
+  $ciudad = $_POST['ciudad'];
   $nivel_usuario = 2;
   $dirid = 1;
 
-  // analiza si se subió imagen
+  /* 
+  Si el user subió imagen, se inserta la misma en la tabla correspondiente,
+  posteriormente se toma el id de dicho registro. 
+  Si no, se inserta el id default (2) de una imagen precargada en la bd.
+  */
   if (is_uploaded_file($_FILES['inpFile']['tmp_name'])) {
-    // sube imagen
     $imageHandler->insertImagen();
   }
 
-  $query = "INSERT INTO $USER_TABLE
+  /* Inserta los valores de direccion en la tabla correspondiente*/
+  $sql = "INSERT INTO $DIRECCION_TABLE (ciudad, estado)
+          VALUES (:ciudad, :estado)";
+  $valores = [':ciudad' => $ciudad, ':estado' => $estado,];
+  $declaracion = $pdo->prepare($sql);
+  if ($declaracion->execute($valores)) {
+    $dirid = $pdo->lastInsertId();
+
+    /* Inserta en la tabla de usuario*/
+    $query = "INSERT INTO $USER_TABLE
       (apodo, nom, app, apm, correo, cont, fecnac, arid, esp, telefono, sexo, rfc, ine, descripcion, nivid, foto, fblink, dirid)
       VALUES (:apodo, :nom, :app, :apm, :correo, :cont, :fecnac, :arid, :esp,
       :tel, :sexo, :rfc, :ine, :des, :nivus, :foto, :fblink, :dirid)";
-  // Arreglo asociativo con valores para execute()
-  // Une el Script SQL con los datos
-  $binding = [
-    ':apodo' => $apodo,
-    ':nom' => $firsname,
-    ':app' => $lastnamep,
-    ':apm' => $lastnamem,
-    ':correo' => $email,
-    ':cont' => $contr,
-    ':fecnac' => $fecnaci,
-    ':arid' => $arids,
-    ':esp' => $espid,
-    ':tel' => $tel,
-    ':sexo' => $sex,
-    ':rfc' => $rfc,
-    ':ine' => $ine,
-    ':des' => $des,
-    ':nivus' => $nivel_usuario,
-    ':foto' => $imageHandler->getId(),
-    ':fblink' => $facebook,
-    ':dirid' => $dirid
-  ];
-  // Declaración preparada (evita inyecciones SQL)
-  $stmt = $pdo->prepare($query);
+    // Arreglo asociativo con valores para execute()
+    // Une el Script SQL con los datos
+    $binding = [
+      ':apodo' => $apodo,
+      ':nom' => $firsname,
+      ':app' => $lastnamep,
+      ':apm' => $lastnamem,
+      ':correo' => $email,
+      ':cont' => $contr,
+      ':fecnac' => $fecnaci,
+      ':arid' => $arids,
+      ':esp' => $espid,
+      ':tel' => $tel,
+      ':sexo' => $sex,
+      ':rfc' => $rfc,
+      ':ine' => $ine,
+      ':des' => $des,
+      ':nivus' => $nivel_usuario,
+      ':foto' => $imageHandler->getId(),
+      ':fblink' => $facebook,
+      ':dirid' => $dirid
+    ];
+    // Declaración preparada (evita inyecciones SQL)
+    $stmt = $pdo->prepare($query);
 
-  // Ejecuta la declaración
-  if ($stmt->execute($binding)) {
-    $jsonRes['status'] = true;
-  } else {
-    $jsonRes['msg'] = $pdo->errorInfo();
+    // Ejecuta la declaración
+    if ($stmt->execute($binding)) {
+      $jsonRes['status'] = true;
+    } else {
+      $jsonRes['msg'] = $pdo->errorInfo();
+    }
   }
 }
 echo json_encode($jsonRes);
